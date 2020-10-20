17558e215641f74a7a54ab47ce56dac602d26146
Mirelle Nascimento <mmirellenascimento@gmail.com>
2020-10-20 17:53:40 +0200

    JS and HTML Parte 02

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ console.log(km)
 var age = prompt ("Quanti anni hai?");
 console.log(age)
 
+
 var kmPrice = 0.21;
+
 var discountUnder18 = 0.80;
+var priceUnder18 = km * kmPrice * discountUnder18;
+priceUnder18.toFixed(2);
+
 var discountOver65 = 0.60;
+var priceOver65 = km * kmPrice * discountOver65;
+priceOver65.toFixed(2);
+
+var normalPrice = km * kmPrice;
+normalPrice.toFixed(2);
 
 
 if (isNaN(km) || isNaN(age)){
@@ -14,16 +24,15 @@ if (isNaN(km) || isNaN(age)){
 }
 else {
   if (age < 18){
-    document.getElementById('price').innerHTML = km * kmPrice * discountUnder18 + " €";
-    console.log(km * kmPrice * discountUnder18 + " €");
+    document.getElementById('price').innerHTML = priceUnder18 + " €";
+    console.log( priceUnder18 + " €");
   }
   else if (age > 64){
-    document.getElementById('price').innerHTML = km * kmPrice * discountOver65 + " €";
+    document.getElementById('price').innerHTML = priceOver65 + " €";
     console.log(km * kmPrice * discountOver65 + " €");
   }
   else{
-    document.getElementById('price').innerHTML = km * kmPrice + " €";
-    console.log(km * kmPrice + " €");
+    document.getElementById('price').innerHTML = normalPrice + " €";
+    console.log(normalPrice + " €");
   }
-
 }
