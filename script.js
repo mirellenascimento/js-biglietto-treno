aed81bacc4ee2a8a89c39a6cf90e5e9a59e2fa00
Mirelle Nascimento <mmirellenascimento@gmail.com>
2020-10-21 19:57:58 +0200

    New Version - Part 06

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
+// Fixed values
 var KM_PRICE = 0.21;
 var DISCOUNT_UNDER = 0.20;
 var DISCOUNT_OVER = 0.40;
 
+// Variables
 var name;
 var km;
 var discount;
 var carriageNumber = Math.floor(Math.random() * (12 - 1)) + 1;
-var ticketCode = Math.floor(Math.random() * (100000 - 10000)) + 100000;
+var ticketCode = Math.floor(Math.random() * (1000000 - 100000)) + 100000;
 
 
-var fullPrice = km * KM_PRICE;
-var underPrice = fullPrice -= fullPrice * DISCOUNT_UNDER;
-var overPrice = fullPrice -= fullPrice * DISCOUNT_OVER;
-
-
+// Buttons
 var generateButton = document.getElementById("genera");
 var cleanButton = document.getElementById("annulla");
 
 
-// Buttons Action
+// Button GENERA Action
+
 
 generateButton.addEventListener("click", function(){
-  // var visitatore;
+// imput data
   var name = document.getElementById("nomeECognome").value;
   console.log(name);
   var km = document.getElementById("kmDaPercorrere").value;
@@ -29,68 +28,45 @@ generateButton.addEventListener("click", function(){
   var discount = document.getElementById("age").value;
   console.log(discount);
 
+// prices
+  var fullPrice = parseInt(km) * KM_PRICE;
+  var underPrice = fullPrice -= fullPrice * DISCOUNT_UNDER;
+  var overPrice = fullPrice -= fullPrice * DISCOUNT_OVER;
 
-
+// ouput data
+// General
   document.getElementById("nome").innerHTML = name;
-
-
-  if (discount == "under18"){
-    document.getElementById("discountApplied").innerHTML = "Sconto Minorenne";
-  } else if (discount == "over65"){
-    document.getElementById("discountApplied").innerHTML = " Sconto Over 65";
-  } else {
-    document.getElementById("discountApplied").innerHTML = " Senza Sconto";
-  }
-
-
-
   document.getElementById("carriage").innerHTML = carriageNumber;
   document.getElementById("codice").innerHTML = ticketCode;
 
 
+// Discount and Price
+  if (isNaN(fullPrice)){
+    alert("Non hai inserito i valori!");
+    document.getElementById("outputText").style.display = "none";
+    document.getElementById("outputBlock").style.display = "none";
+  } else {
+    if (discount == "under18"){
+      document.getElementById("discountApplied").innerHTML = "Sconto Minorenne";
+      document.getElementById("finalPrice").innerHTML = underPrice.toFixed(2) + " €" ;
+      document.getElementById("outputText").style.display = "block";
+      document.getElementById("outputBlock").style.display = "block";
+    } else if (discount == "over65"){
+      document.getElementById("discountApplied").innerHTML = " Sconto Over 65";
+      document.getElementById("finalPrice").innerHTML = overPrice.toFixed(2) + " €" ;
+      document.getElementById("outputText").style.display = "block";
+      document.getElementById("outputBlock").style.display = "block";
+    } else {
+      document.getElementById("discountApplied").innerHTML = " Senza Sconto";
+      document.getElementById("finalPrice").innerHTML = fullPrice.toFixed(2) + " €" ;
+      document.getElementById("outputText").style.display = "block";
+      document.getElementById("outputBlock").style.display = "block";
+    }
+  }
+
+
+
+
+
+
  });
- // if (under){
- //   document.getElementById("siacountApplied").innerHTML = "Sconto Minnorene";
- // } else if (over){
- //   document.getElementById("siacountApplied").innerHTML = "Sconto Over 65";
- // } else {
- //   document.getElementById("siacountApplied").innerHTML = "Senza Sconto";
- // }
-
-
-
-
-//
-//
-//  if (isNaN(km) {
-//    alert("I numeri inseriti non sono validi");
-//    console.log("I numeri non sono validi")
-//  }
-//  else {
-//    if (under){
-//      document.getElementById('finalPrice').innerHTML = underPrice.toFixed(2) + " €";
-//      console.log(underPrice.toFixed(2) + " €");
-//    }
-//    else if (age > 64){
-//      document.getElementById('price').innerHTML = priceOver65 + " €";
-//      console.log(km * kmPrice * discountOver65 + " €");
-//    }
-//    else{
-//      document.getElementById('price').innerHTML = normalPrice + " €";
-//      console.log(normalPrice + " €");
-//    }
-//  }
-//
-//
-//  var formulaUnder18 = km * kmPrice * discountUnder18;
-//  priceUnder18 = formulaUnder18.;
-//
-//
-// var formulaUnder18 = km * kmPrice * discountUnder18;
-// priceUnder18 = formulaUnder18.toFixed(2);
-//
-// var formulaOver65 = km * kmPrice * discountOver65;
-// priceOver65 = formulaOver65.toFixed(2);
-//
-// var noDiscount = km * kmPrice;
-// normalPrice = noDiscount.toFixed(2);
